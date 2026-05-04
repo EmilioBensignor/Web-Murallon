81e7e0d67dfdd5efd1a1a9957ef730c5575ab53b
Emilio <liobensignor@gmail.com>
2026-05-04 15:27:31 -0300

    Blogs con v-if

--- app/components/home/Blog.vue
+++ app/components/home/Blog.vue
@@ -1,5 +1,5 @@
 <template>
-    <DefaultSection class="bg-primary py-6 md:p-8 lg:p-16">
+    <DefaultSection v-if="posts.length" class="bg-primary py-6 md:p-8 lg:p-16">
         <div class="md:w-full md:flex md:justify-between md:items-center">
             <div class="flex flex-col md:flex-row items-center md:gap-2">
                 <HeadingH2 class="text-white uppercase">Nuestro Blog</HeadingH2>
@@ -10,7 +10,7 @@
             <CarouselArrows @prev="carouselRef?.scrollLeft()" @next="carouselRef?.scrollRight()" />
         </div>
 
-        <CarouselStatic v-if="posts.length" :slides-per-view="{ base: 1.4, sm: 1.6, tab: 1.8, md: 1.8, lg: 1.8, xl: 2.2, xxl: 2.4 }"
+        <CarouselStatic :slides-per-view="{ base: 1.4, sm: 1.6, tab: 1.8, md: 1.8, lg: 1.8, xl: 2.2, xxl: 2.4 }"
             :gap="16" :show-dots="{ base: true, lg: false }" :show-arrows="{ base: false, lg: true }">
             <BlogCard v-for="post in posts" :key="post.id" :post="post"
                 :imagen-url="getImageUrl(post.imagen_principal)" />
